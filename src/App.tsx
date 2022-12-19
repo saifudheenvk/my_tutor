5dff51c871936201d811274828a945d97b0c031d
saifudheen <saifudheen@innovatise.com>
2022-12-19 23:35:55 +0530

    default style changed to light

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,16 @@ import { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import './App.css';
 import Content from './components/Content';
-import { changeTheme } from './redux/actions/theme';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { useAppDispatch, useAppSelector } from './redux/store';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import GlobalStyles from './utils/GlobalStyles';
 import theme from './utils/theme';
-import theme_two from './theme_two';
-import theme_one from './theme_one';
 import LoginForm from './components/Login/LoginForm';
 import ForgotPassword from './components/Login/ForgotPassword';
-
-declare const window: any;
+import { useAppSelector } from './redux/store';
 
 function App() {
-
-  const dispatch = useAppDispatch();
   const themeRedux = useAppSelector(state=>state.themeReducer)
-
-  const themeSelector = (themeState: string) => {
-    if (themeState !== 'white') {
-      dispatch(changeTheme('dark'));
-      window.less
-        .modifyVars({
-          ...theme_two,
-        })
-        .then(() => {
-          console.log('success');
-        })
-        .catch(() => {
-          console.log('fail');
-        });
-    } else {
-      dispatch(changeTheme('white'));
-      window.less
-        .modifyVars({
-          ...theme_one,
-        })
-        .then(() => {
-          console.log('success');
-        })
-        .catch(() => {
-          console.log('fail');
-        });
-    }
-  };
-
-  useEffect(() => {
-    const themeState = localStorage.getItem('app_theme')|| "dark";
-    themeSelector(themeState)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+ 
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,9 +1,15 @@
 import { Layout } from "antd";
-import { FC, Suspense } from "react";
+import { FC, Suspense, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { useAppSelector } from "../../redux/store";
+import { changeTheme } from "../../redux/actions/theme";
+import { useAppDispatch, useAppSelector } from "../../redux/store";
+import theme_one from "../../theme_one";
+import theme_two from "../../theme_two";
 import Header from "../Header";
 import RouteMap from "./RouteMap";
+
+declare const window: any;
 
 const { Content: AntdContent } = Layout;
 
@@ -42,7 +48,48 @@ interface IProps {
 
 const Content: FC<IProps> = (props) => {
 
-  const themeRedux = useAppSelector(state => state.themeReducer)
+  const dispatch = useAppDispatch();
+  const themeRedux = useAppSelector(state=>state.themeReducer)
+
+  const themeSelector = (themeState: string) => {
+    if (themeState !== 'white') {
+      dispatch(changeTheme('dark'));
+      window.less
+        .modifyVars({
+          ...theme_two,
+        })
+        .then(() => {
+          console.log('success');
+        })
+        .catch(() => {
+          console.log('fail');
+        });
+    } else {
+      dispatch(changeTheme('white'));
+      window.less
+        .modifyVars({
+          ...theme_one,
+        })
+        .then(() => {
+          console.log('success');
+        })
+        .catch(() => {
+          console.log('fail');
+        });
+    }
+  };
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const themeState = localStorage.getItem('app_theme')|| "dark";
+    themeSelector(themeState)
+
+    const token = localStorage.getItem("auth_token");
+    if(!token){
+      navigate('/sign-in')
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <ContainerLayout theme={themeRedux} >
         <Header />
